ce775a4a72cdae70668b1e7b46c1b34801d767da
KnIfER <knziha@gmail.com>
2020-10-12 13:46:04 +0800

    add dependencies

--- MarkdownEngines/main.js
+++ MarkdownEngines/main.js
@@ -18,26 +18,11 @@
 	function init(name){
 		console.log(name.src);
 		var src_url=name.src+"/../";
-		var jss=["jquery.js"
-		, "marked.js"
-		, "highlight.js"
-		, "config.js"
-		, "markdownify.js"
-		, "webfont.js"
-		, "snap.svg.js"
-		, "underscore.js"
-		, "sequence-diagram.js"
-		, "raphael.js"
-		, "flowchart.js"
-		, "diagramflowseq.js"
-		, "katex.js"
-		, "rawdeflate.js"
-		, "platumlencode.js"
-		, "mermaid.js" ];
+		var jss=["jquery.js", "marked.js", "highlight.js", "config.js", "markdownify.js", "webfont.js", "snap.svg-min.js", "underscore-min.js", "sequence-diagram-min.js", "raphael.min.js", "flowchart.min.js", "diagramflowseq.js", "katex.min.js", "rawdeflate.js", "platumlencode.js", "mermaid.min.js" ];
 		for(var j in jss){
 			loadJs(src_url+jss[j], j==jss.length-1);
 		}
-		jss=["Clearness.css", "katex.css"];
+		jss=["Clearness.css", "katex.min.css"];
 		for(var j in jss){
 			loadCss(src_url+"theme/"+jss[j]);
 		}
