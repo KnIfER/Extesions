1e626bf1a89a4b70348be69fab2efe633fa0833f
KnIfER <knziha@gmail.com>
2020-10-16 17:04:09 +0800

    reduce: markdownify.js( 取消独立资格，并入main.js )

--- MarkdownEngines/MDViewer/main.js
+++ MarkdownEngines/MDViewer/main.js
@@ -1,3 +1,42 @@
+(function(document) {
+    function resolveImg(img) {
+        var src = $(img).attr("src");
+        if (src[0] == "/") {
+            $(img).attr("src", src.substring(1));
+        }
+    }
+
+    function makeHtml(data) {
+		var items={};
+		//items.supportMath=1;
+		items.katex=1;
+		items.html=1;
+		items.toc=1;
+		// Convert MarkDown to HTML
+		var preHtml = data;
+		if (items.html) {
+			config.markedOptions.sanitize = false;
+		}
+		if (items.katex) {
+			config.markedOptions.katex = true;
+			preHtml = diagramFlowSeq.prepareDiagram(data);
+		}
+		marked.setOptions(config.markedOptions);
+		var html = marked(preHtml);
+		$(document.body).html(html);
+
+		$('img').on("error", function() {
+			resolveImg(this);
+		});
+
+		diagramFlowSeq.drawAllFlow();
+		diagramFlowSeq.drawAllSeq();
+		diagramFlowSeq.drawAllMermaid();
+
+		if(window.MDAP)
+			window.MDAP();
+	}
+	/* 引擎初始化 */
 	function loadJs(name,up){
 		var item = document.createElement('script');
 		item.src=name;
@@ -22,7 +61,6 @@
 		, "marked.js"
 		, "highlight.js"
 		, "config.js"
-		, "markdownify.js"
 		, "raphael.min.js"
 		, "flowchart.min.js"
 		, "diagramflowseq.js"
@@ -37,7 +75,9 @@
 		for(var j in jss){
 			loadCss(src_url+"theme/"+jss[j]);
 		}
-
 		//window.update();
-
 	}
+	window.APMD = makeHtml;
+	window.init = init;
+}(document));
+	
